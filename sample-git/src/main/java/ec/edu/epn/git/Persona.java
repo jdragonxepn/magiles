f98a7e2353a37de539a08ca27b34c9ec98aced1a
jdragonxepn <jonathan.herrera01@epn.edu.ec>
2020-07-14 15:35:11 -0500

    Agregar campos a Persona

--- sample-git/src/main/java/ec/edu/epn/git/Persona.java
+++ sample-git/src/main/java/ec/edu/epn/git/Persona.java
@@ -3,20 +3,35 @@ package ec.edu.epn.git;
 public class Persona {
 	private int age;
 	private String name;
-	public Persona(int age, String name) {
+	private String city;
+	
+	public Persona(int age, String name, String city) {
 		super();
 		this.age = age;
 		this.name = name;
+		this.city = city;
 	}
+	
+	public String getCity() {
+		return city;
+	}
+	
+	public void setCity(String city) {
+		this.city = city;
+	}
+	
 	public int getAge() {
 		return age;
 	}
+	
 	public void setAge(int age) {
 		this.age = age;
 	}
+	
 	public String getName() {
 		return name;
 	}
+	
 	public void setName(String name) {
 		this.name = name;
 	}
